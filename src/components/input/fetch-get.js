06ab1718727ecd477301616442ed3e40f60a98eb
Elena Slyusareva <sportgirl55@bk.ru>
2021-08-31 19:50:32 +0500

    Child items have become beautiful

--- src/components/input/fetch-get.js
+++ src/components/input/fetch-get.js
@@ -1,6 +1,4 @@
 export class FetchGet {
-  message = "У подразделения нет дочерних элементов";
-
   _baseUrl = "https://wfm-dev.t.goodt.me/api/v1/org-units";
   _headers = {
     method: "GET",
@@ -10,6 +8,10 @@ export class FetchGet {
   };
   orgUnits;
   children;
+  statusChild;
+
+  loading = false;
+  // error = false;
 
   attached() {
     fetch(`${this._baseUrl}/main`, this._headers)
@@ -17,23 +19,29 @@ export class FetchGet {
       .then((data) => {
         const orgUnits = data._embedded.orgUnits;
         this.orgUnits = orgUnits;
-      });
+      })
+     
   }
 
   getName(item) {
     return item.name;
   }
   getChildren = async (elem) => {
+  
     if (elem._links.children) {
+      this.loading = true;
       await fetch(`${this._baseUrl}/${elem.id}/children`, this._headers)
+
         .then((response) => response.json())
         .then((data) => {
           const children = data._embedded.orgUnits;
           this.children = children;
+          this.statusChild = true;
           console.log(children);
-        });
+        })
+         .finally(this.loading = false)
     } else {
-      console.log("Нет подразделений для этого элемента");
+      this.statusChild = false;
     }
   };
 }
